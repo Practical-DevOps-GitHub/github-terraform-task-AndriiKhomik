cf932652b886b049c621d248495ba26e51d8f068
Andrii <andrii.khomik@gmail.com>
2024-09-20 19:41:17 +0300

    initial

---
+++ src/variables.tf
@@ -0,0 +1,31 @@
+variable "token" {
+  description = "Token to Github"
+  type        = string
+}
+
+variable "github_organization" {
+  description = "github_organization"
+  type        = string
+}
+
+variable "username" {
+  description = "username"
+  type        = string
+}
+
+variable "repository_name" {
+  description = "Repository name"
+  type        = string
+}
+
+variable "collabprator_permission" {
+  description = "Collaborator permission"
+  type        = string
+}
+
+variable "branch" {
+  description = "Branch"
+  type        = string
+}
+
+
